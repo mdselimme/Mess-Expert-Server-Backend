5cb4752bd396278120c17e99dc1acac6f4161030
afk-jisan <baktier.galib.jisan@gmail.com>
2025-08-11 04:54:49 +0600

    cors fix?

--- src/app/app.js
+++ src/app/app.js
@@ -7,7 +7,9 @@ app.use(cookieParser());
 //middleware
 app.use(cors({
     origin: ["https://mess-expert-cline-site.vercel.app", "http://localhost:5173", "https://mess-expert-server-backend.onrender.com"],
-    credentials: true
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"]
 }));
 const globalErrorHandler = require('../middleware/globalErrorHandler');
 const notFoundRoute = require('../middleware/notFound');
